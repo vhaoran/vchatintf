61d97c3099272b0f97f9a6135877685301a21265
whr <whr@whr.local>
2020-01-16 14:58:35 +0800

    whr

--- msg/refmsg/msgHisRef.go
+++ msg/refmsg/msgHisRef.go
@@ -8,23 +8,24 @@ import (
 type MsgHisRef struct {
 	ID        primitive.ObjectID `json:"id,omitempty"   bson:"_id,omitempty"`
 	CreatedAt ytime.Date         `json:"created_at,omitempty"   bson:"created_at,omitempty"`
-	//发送方id
-	FromUID int64 `json:"from_uid,omitempty"   bson:"from_uid,omitempty"`
-
 	//0: 私信消息
 	//1：群消息
 	//2：朋友圈消息
 	//3：系统消息(如某用户资料变更)
 	MsgType int `json:"msg_type,omitempty"   bson:"msg_type,omitempty"`
 
+	//发送方id
+	FromUID int64 `json:"from_uid,omitempty"   bson:"from_uid,omitempty"`
+
 	//目标用户ID
-	ToUID       int64  `json:"to_uid,omitempty"   bson:"to_uid,omitempty"`
-	UserCodeRef string `json:"usercdde_ref,omitempty"   bson:"usercdde_ref,omitempty"`
-	NickRef     string `json:"nick_ref,omitempty"   bson:"nick_ref,omitempty"`
-	IconRef     string `json:"icon_ref,omitempty"   bson:"icon_ref,omitempty"`
-	ToBID       int64  `json:"to_bid,omitempty"   bson:"to_bid,omitempty"`
-	BNameRef    string `json:"bname_ref,omitempty"   bson:"bname_ref,omitempty"`
-	BIconRef    string `json:"bicon_ref,omitempty"   bson:"bicon_ref,omitempty"`
+	ToUID         int64  `json:"to_uid,omitempty"   bson:"to_uid,omitempty"`
+	ToUserCodeRef string `json:"to_user_code_ref,omitempty"   bson:"to_user_code_ref,omitempty"`
+	ToNickRef     string `json:"to_nic_ref,omitempty"   bson:"to_nic_ref,omitempty"`
+	ToIconRef     string `json:"to_icon_ref,omitempty"   bson:"to_icon_ref,omitempty"`
+
+	ToGID      int64 `json:"to_gid,omitempty"   bson:"to_gid,omitempty"`
+	ToGNameRef string `json:"to_gname_ref,omitempty"   bson:"to_gname_ref,omitempty"`
+	ToGIconRef string `json:"to_gicon_ref,omitempty"   bson:"to_gicon_ref,omitempty"`
 	//自定义的消息内容类型，可选
 	BodyType int `json:"body_type,omitempty"   bson:"body_type,omitempty"`
 
