596a9633f93a220be6b4204f06e9f6cfdfcab72f
whr <whr@w520>
2020-01-20 12:30:47 +0800

    whr

--- msg/trigger-online-msg.go
+++ msg/trigger-online-msg.go
@@ -2,34 +2,33 @@ package msg
 
 //for snippet用于标准返回值的微服务接口
 
+//for snippet用于标准返回值的微服务接口
+
 import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"net/http"
-
 	"github.com/davecgh/go-spew/spew"
 	"github.com/go-kit/kit/endpoint"
 	tran "github.com/go-kit/kit/transport/http"
-	"github.com/vhaoran/vchat/common/ypage"
+	"github.com/vhaoran/vchat/common/ytime"
 	"github.com/vhaoran/vchat/lib/ykit"
+	"net/http"
 )
 
 const (
-	//todo
-	PageMsgHis_HANDLER_PATH = "/PageMsgHis"
+	TriggerOnLine_HANDLER_PATH = "/TriggerOnLine"
 )
 
 type (
-	MsgHisPageService interface {
-		//todo
-		Exec(in *PageMsgHisIn) (*ykit.Result, error)
+	TriggerOnLineService interface {
+		Exec(in *TriggerOnLineIn) (*ykit.Result, error)
 	}
 
 	//input data
-	//todo
-	PageMsgHisIn struct {
-		ypage.PageBean
+	TriggerOnLineIn struct {
+		UID         int64      `json:"uid omitempty"`
+		LastAckTime ytime.Date `json:"last_ack_time omitempty"`
 	}
 
 	//output data
@@ -40,29 +39,28 @@ type (
 	//}
 
 	// handler implements
-	PageMsgHisHandler struct {
+	TriggerOnLineHandler struct {
 		base ykit.RootTran
 	}
 )
 
-func (r *PageMsgHisHandler) MakeLocalEndpoint(svc MsgHisPageService) endpoint.Endpoint {
+func (r *TriggerOnLineHandler) MakeLocalEndpoint(svc TriggerOnLineService) endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		fmt.Println("#############  PageMsgHis ###########")
+		fmt.Println("#############  TriggerOnLine ###########")
 		spew.Dump(ctx)
 
-		//todo
-		in := request.(*PageMsgHisIn)
+		in := request.(*TriggerOnLineIn)
 		return svc.Exec(in)
 	}
 }
 
 //个人实现,参数不能修改
-func (r *PageMsgHisHandler) DecodeRequest(ctx context.Context, req *http.Request) (interface{}, error) {
-	return r.base.DecodeRequest(new(PageMsgHisIn), ctx, req)
+func (r *TriggerOnLineHandler) DecodeRequest(ctx context.Context, req *http.Request) (interface{}, error) {
+	return r.base.DecodeRequest(new(TriggerOnLineIn), ctx, req)
 }
 
 //个人实现,参数不能修改
-func (r *PageMsgHisHandler) DecodeResponse(_ context.Context, res *http.Response) (interface{}, error) {
+func (r *TriggerOnLineHandler) DecodeResponse(_ context.Context, res *http.Response) (interface{}, error) {
 	var response ykit.Result
 	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
 		return nil, err
@@ -71,7 +69,7 @@ func (r *PageMsgHisHandler) DecodeResponse(_ context.Context, res *http.Response
 }
 
 //handler for router，微服务本地接口，
-func (r *PageMsgHisHandler) HandlerLocal(service MsgHisPageService,
+func (r *TriggerOnLineHandler) HandlerLocal(service TriggerOnLineService,
 	mid []endpoint.Middleware,
 	options ...tran.ServerOption) *tran.Server {
 
@@ -90,16 +88,25 @@ func (r *PageMsgHisHandler) HandlerLocal(service MsgHisPageService,
 }
 
 //sd,proxy实现,用于etcd自动服务发现时的handler
-func (r *PageMsgHisHandler) HandlerSD(mid []endpoint.Middleware,
+func (r *TriggerOnLineHandler) HandlerSD(mid []endpoint.Middleware,
 	options ...tran.ServerOption) *tran.Server {
 	return r.base.HandlerSD(
 		context.Background(),
 		MSTAG,
-		//todo
 		"POST",
-		PageMsgHis_HANDLER_PATH,
+		TriggerOnLine_HANDLER_PATH,
 		r.DecodeRequest,
 		r.DecodeResponse,
 		mid,
 		options...)
+}
+
+func (r *TriggerOnLineHandler) ProxySD() endpoint.Endpoint {
+	return r.base.ProxyEndpointSD(
+		context.Background(),
+		MSTAG,
+		"POST",
+		TriggerOnLine_HANDLER_PATH,
+		r.DecodeRequest,
+		r.DecodeResponse)
 }
